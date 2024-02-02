872cec11bda625e721968f1a2fef6fd1c6669a4e
Rob Simmons <robsimmons@gmail.com>
2024-02-02 18:15:22 -0500

    Initial commit - static site and service

---
+++ src/main.ts
@@ -0,0 +1,49 @@
+let incremental: boolean = false;
+
+const primaryView = document.getElementById(
+  "primary-view"
+) as HTMLTextAreaElement;
+
+const incrementalView = document.getElementById(
+  "incremental-view"
+) as HTMLTextAreaElement;
+
+const twelfResponse = document.getElementById(
+  "twelf-response"
+) as HTMLTextAreaElement;
+
+const incrementalToggle = document.getElementById(
+  "show-incremental-view"
+) as HTMLInputElement;
+
+const checkButton = document.getElementById(
+  "check-button"
+) as HTMLButtonElement;
+
+incrementalToggle.onchange = () => {
+  incremental = incrementalToggle.checked;
+  document.getElementById("incremental-view-wrap")!.className = incremental
+    ? "wrap"
+    : "wrap-hidden";
+};
+
+checkButton.onclick = () => {
+  twelfResponse.value = "";
+  const body = incremental
+    ? `${primaryView.value}\0${incrementalView.value}`
+    : primaryView.value;
+  console.log(body);
+  fetch("https://twelflive.onrender.com/eval", { method: "POST", body })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.error) {
+        twelfResponse.value = response.msg;
+      } else {
+        twelfResponse.value = `${response.output}\n${
+          response.killed ? "%% TIMEOUT %%" : response.server
+        }`;
+      }
+    });
+};
+
+console.log("Hey");
